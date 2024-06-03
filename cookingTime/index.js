df913b41ba7ae28e80e2cf30316ffdffdcfb14fb
itsneedtoknow <letta_in_love@mail.ru>
2024-06-03 22:20:35 +0300

    feat: initialize the cookingTime task

---
+++ cookingTime/index.js
@@ -0,0 +1,11 @@
+// You've purchased a ready-meal from the supermarket.
+
+// The packaging says that you should microwave it for 4 minutes and 20 seconds, based on a 600W microwave.
+
+// Oh no, your microwave is 800W! How long should you cook this for?!
+
+function cookingTime(neededPower, minutes, seconds, power) {
+    // Your code here
+  }
+
+  cookingTime("600W", 4, 20, "800W")
